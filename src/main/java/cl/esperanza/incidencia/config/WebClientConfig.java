b702c1b48ff2271d21dfbcebdd933847bbc9d60f
crosral <jeral.melillancachavez@gmail.com>
2026-05-29 23:04:57 -0400

    update - cambie el properties y el webclient para que el servicio de incidencias se ejecute en el puerto 8085 y se conecte al servicio de usuarios en el puerto 8080

--- src/main/java/cl/esperanza/incidencia/config/WebClientConfig.java
+++ src/main/java/cl/esperanza/incidencia/config/WebClientConfig.java
@@ -1,15 +1,17 @@
 package cl.esperanza.incidencia.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-    /*
+
     @Bean
-    public WebClient reparacionesWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8081/api/v1/reparaciones"); 
+    public WebClient reparacionesWebClient() {
+       
+        return WebClient.builder()
+                .baseUrl("http://localhost:8088/api/v1/reparaciones")
+                .build();
     }
-    
-    */
-    
 }
