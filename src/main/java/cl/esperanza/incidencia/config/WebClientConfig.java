789b93b1ff40cda6109ffac67242cb774bd7a1eb
CyCklop <ignacioandresflores06@gmail.com>
2026-07-02 11:22:49 -0400

    modificado los configs

--- src/main/java/cl/esperanza/incidencia/config/WebClientConfig.java
+++ src/main/java/cl/esperanza/incidencia/config/WebClientConfig.java
@@ -3,16 +3,17 @@ package cl.esperanza.incidencia.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient reparacionesWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8088/api/v1/reparaciones").build();
+    public WebClient reparacionesWebClient(@Value("${reparaciones.service.url:http://localhost:8088/api/v1/reparaciones}") String reparacionServiceUrl) {
+        return WebClient.builder().baseUrl(reparacionServiceUrl).build();
     }
 
     @Bean
-    public WebClient sociosWebClient() {
-        return WebClient.builder().baseUrl("http://localhost:8082").build();
+    public WebClient sociosWebClient(@Value("${socios.service.url:http://localhost:8082/api/v1/socios}") String sociosServiceUrl) {
+        return WebClient.builder().baseUrl(sociosServiceUrl).build();
     }
 }
